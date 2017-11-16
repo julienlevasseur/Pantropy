d1ab9640214d5fbc80f4278f165df0508ee2e217
Julien Levasseur <dko2327@lab-vfbl5831-0.mvt.desjardins.dev>
2017-11-16 14:06:13 -0500

    improve tests

---
+++ api/handlers_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+)
+
+func RouterIndexTest(route string, method string) *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc(route, Index).Methods(method)
+	return router
+}
+
+func TestHandlerIndex(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/v1", nil)
+	response := httptest.NewRecorder()
+	RouterIndexTest("/v1", "GET").ServeHTTP(response, request)
+	assert.Equal(t, 200, response.Code, "OK response is expected")
+	if response.Code == http.StatusOK {
+		displayStatusOK("Index", response.Code)
+	}
+}
+
+func RouterProvIndexTest(route string, method string) *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc(route, ProvidersIndex).Methods(method)
+	return router
+}
+
+func TestHandlerProvIndex(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/v1/providers", nil)
+	response := httptest.NewRecorder()
+	RouterProvIndexTest("/v1/providers", "GET").ServeHTTP(response, request)
+	assert.Equal(t, 200, response.Code, "OK response is expected")
+	if response.Code == http.StatusOK {
+		displayStatusOK("ProvidersIndex", response.Code)
+	}
+}
