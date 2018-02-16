bd5ed412d32eb15041e49114d1fb63b9f8427275
Julien Levasseur <levasju1@LAP-MTL-LEVASJU.vasco.com>
2018-02-16 13:58:57 -0500

    correct Line 16: warning: don't use underscores in Go names; func parameter obj_type should be objType (golint)

--- api/common_test.go
+++ api/common_test.go
@@ -13,8 +13,8 @@ func displayBodyOK(route string, body string) {
 	fmt.Printf(color.GreenString("%v%v%v%v\n"), "[OK] ", route, " body: ", body)
 }
 
-func displayTypeOK(obj string, obj_type string) {
-	fmt.Printf(color.GreenString("%v%v%v%v\n"), "[OK] ", obj, " type: ", obj_type)
+func displayTypeOK(obj string, objType string) {
+	fmt.Printf(color.GreenString("%v%v%v%v\n"), "[OK] ", obj, " type: ", objType)
 }
 
 func PrintGreen(content string) {
